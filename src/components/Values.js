ff955ffa88040cf79adf52f06a2d9dca7de4b34a
AbhishekBhavnani2003 <abhishekbhavnani2003@gmail.com>
2024-04-07 13:42:33 +0530

    Commit

--- src/components/Values.js
+++ src/components/Values.js
@@ -3,16 +3,16 @@ import './Values.css'
 function Values() {
   return (
     <div>
-<div class="py-16 bg-white" style={{marginTop:'-300px'}}>  
-  <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-      <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-        <div class="md:5/12 lg:w-5/12 visionimage">
+<div className="py-16 bg-white" style={{marginTop:'-300px'}}>  
+  <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+      <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+        <div className="md:5/12 lg:w-5/12 visionimage">
           <img src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="image" loading="lazy" width="" height=""  className=''/>
         </div>
-        <div class="md:7/12 lg:w-6/12">
-          <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Our Vision</h2>
-          <p class="mt-6 text-gray-600 " style={{fontWeight:'bold'}}><span>"Empowering a World Without Communication Barriers"</span></p>
-          <p class="mt-4 text-gray-600">Our vision is to leverage cutting-edge technology to empower individuals with hearing, speech, or visual impairments, fostering a more inclusive and accessible world. We envision a future where our smart glasses become a bridge to communication, independence, and endless possibilities, allowing everyone, regardless of their abilities, to connect, express themselves, and thrive in our interconnected society.</p>
+        <div className="md:7/12 lg:w-6/12">
+          <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">Our Vision</h2>
+          <p className="mt-6 text-gray-600 " style={{fontWeight:'bold'}}><span>"Empowering a World Without Communication Barriers"</span></p>
+          <p className="mt-4 text-gray-600">Our vision is to leverage cutting-edge technology to empower individuals with hearing, speech, or visual impairments, fostering a more inclusive and accessible world. We envision a future where our smart glasses become a bridge to communication, independence, and endless possibilities, allowing everyone, regardless of their abilities, to connect, express themselves, and thrive in our interconnected society.</p>
         </div>
       </div>
   </div>
